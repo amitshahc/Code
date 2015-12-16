e1b437c5205f23958f913c7b1bc91232164a145e
Amit Shah <amitshahc@hotmail.com>
2015-12-16 12:40:27 +0800

    Update autoResize-iFrame.js
    proper indent

--- autoResize-iFrame.js
+++ autoResize-iFrame.js
@@ -9,21 +9,21 @@ function autoResize(id){
     if(document.getElementById){
         newheight=document.getElementById(id).contentWindow.document.body.scrollHeight; 
         //newwidth=document.getElementById(id).contentWindow.document.body.scrollWidth;
-		newheightVisible = newheight + 30; 
-		//console.log("new "+newheight);
-		//console.log("new Visible "+newheightVisible);
+	newheightVisible = newheight + 30; 
+	//console.log("new "+newheight);
+	//console.log("new Visible "+newheightVisible);
     }
 	
 	//console.log("last old "+lastheight);
 	
 	//if(newheight != lastheight && newheightVisible != lastheightVisible)
-	if(newheight != lastheight)
+    if(newheight != lastheight)
     {
-		//document.getElementById(id).height= (500) + "px";
-		document.getElementById(id).height= (newheightVisible) + "px";
+	//document.getElementById(id).height= (500) + "px";
+	document.getElementById(id).height= (newheightVisible) + "px";
     	//document.getElementById(id).width= (newwidth) + "px";	
-		lastheight = newheightVisible;
-		//lastheightVisible = newheightVisible;
-		//console.log("last new "+lastheight);
-	}
+	lastheight = newheightVisible;
+	//lastheightVisible = newheightVisible;
+	//console.log("last new "+lastheight);
+    }
 }
