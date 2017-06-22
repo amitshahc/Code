b553bcfb34ea4cb9ddaa865f265fd74238c5493a
Amit Shah <amitshahc@hotmail.com>
2017-06-22 17:39:47 +0800

    Handle chrome specific change
    get chrome browser height using offsetheight

--- autoResize-iFrame.js
+++ autoResize-iFrame.js
@@ -1,29 +1,24 @@
 var lastheight = 0;
-//var lastheightVisible = 0;
 
-function autoResize(id){
+function autoResize(id) {
     var newheight = 500;
     var newwidth;
 
-	//document.getElementById(id).height= (newheight) + "px";
-    if(document.getElementById){
-        newheight=document.getElementById(id).contentWindow.document.body.scrollHeight; 
+    //jQuery("#" + id).css('height', 'auto');
+
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    //document.getElementById(id).height= (newheight) + "px";
+    if (document.getElementById) {
+        newheight = isChrome ? document.getElementById(id).contentWindow.document.body.offsetHeight : document.getElementById(id).contentWindow.document.body.scrollHeight;
+        //newheight=document.getElementById(id).contentWindow.document.body.scrollHeight; 
         //newwidth=document.getElementById(id).contentWindow.document.body.scrollWidth;
-	newheightVisible = newheight + 30; 
-	//console.log("new "+newheight);
-	//console.log("new Visible "+newheightVisible);
+        newheightVisible = newheight + 30; //30
     }
-	
-	//console.log("last old "+lastheight);
-	
-	//if(newheight != lastheight && newheightVisible != lastheightVisible)
-    if(newheight != lastheight)
-    {
-	//document.getElementById(id).height= (500) + "px";
-	document.getElementById(id).height= (newheightVisible) + "px";
-    	//document.getElementById(id).width= (newwidth) + "px";	
-	lastheight = newheightVisible;
-	//lastheightVisible = newheightVisible;
-	//console.log("last new "+lastheight);
+
+    if (newheight != lastheight) {
+        //document.getElementById(id).height= (500) + "px";
+        //document.getElementById(id).height= (newheightVisible) + "px";
+        jQuery("#" + id).css('height', newheightVisible);
+        lastheight = newheightVisible;
     }
 }
